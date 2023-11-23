5a551f80e1519b752b692bf8e205b7421889a255
ABE19950311 <abe19950311@gmail.com>
2023-11-23 20:00:52 +0900

    17

---
+++ easy/binary-tree-postorder-traversal.js
@@ -0,0 +1,13 @@
+var postorderTraversal = function(root) {
+    if(!root) return []
+    let result = []
+    let stack = [root]
+    while(stack.length) {
+        let node = stack.pop()
+        result.push(node.val)
+        if(node.left) stack.push(node.left)
+        if(node.right) stack.push(node.right)
+    }
+    return result.reverse()
+};
+//binary-tree-preorder-traversalの結果をreverseメソッドで反転
