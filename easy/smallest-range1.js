a8cff6923b0a1538d48c2e7db217a0fff4dc152e
ABE19950311 <abe19950311@gmail.com>
2024-03-20 22:31:04 +0900

    320

---
+++ easy/smallest-range1.js
@@ -0,0 +1,9 @@
+var smallestRangeI = function(nums, k) {
+    let max = Math.max(...nums)
+    let min = Math.min(...nums)
+    let res = (max-min)-2*k
+    return res>0?res:0
+};
+//引数kの値増減して配列の最大値、最小値の差分をとる
+//mathmaxminにスプレッド構文で配列要素渡してmaxmin出す
+//maxminから2*k(max,min二つの差をみる)引いた結果で0からあたいかを返す
