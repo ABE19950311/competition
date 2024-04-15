bcb1f865809810350f44f5d7f22991ec1904d8c7
ABE19950311 <abe19950311@gmail.com>
2024-04-15 21:07:25 +0900

    415

---
+++ easy/create-target-array-in-the-given-order.js
@@ -0,0 +1,8 @@
+var createTargetArray = function(nums, index) {
+    let res = []
+    for(let i=0;i<nums.length;i++) {
+        res.splice(index[i],0,nums[i])
+    }
+    return res
+};
+//spliceメソッドでindexの位置にnumsいれていく
