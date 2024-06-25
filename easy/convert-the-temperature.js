9afd77b893e0d1f7c2033fbcd675c4fba9f49047
ABE19950311 <abe19950311@gmail.com>
2024-06-25 21:17:26 +0900

    625

---
+++ easy/convert-the-temperature.js
@@ -0,0 +1,7 @@
+var convertTemperature = function(celsius) {
+    // let res = []
+    // res.push(celsius+273.15)
+    // res.push(celsius*1.80+32.00)
+    return [celsius+273.15,celsius*1.80+32.00]
+};
+//引数のkelvintofahrehneheitの計算結果を配列にしてかえす
