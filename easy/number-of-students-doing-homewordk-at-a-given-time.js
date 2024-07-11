0ae3a02f5bcfb5feb559f446dba1f66d9f406976
ABE19950311 <abe19950311@gmail.com>
2024-07-11 21:13:35 +0900

    711

---
+++ easy/number-of-students-doing-homewordk-at-a-given-time.js
@@ -0,0 +1,10 @@
+var busyStudent = function(startTime, endTime, queryTime) {
+    let res = 0
+    for(let i=0;i<startTime.length;i++) {
+        if(startTime[i]<=queryTime&&endTime[i]>=queryTime) {
+            res++
+        }
+    }
+    return res
+};
+//starttimeからendTimeの範囲にquerytimeある数カウントしてかえす
