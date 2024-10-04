084bc63ab4afe65ab85a46185375e695ebf4dea7
ABE19950311 <abe19950311@gmail.com>
2024-10-04 20:23:34 +0900

    104

---
+++ easy/js/find-the-maximum-achievable-number.js
@@ -0,0 +1,10 @@
+var theMaximumAchievableX = function(num, t) {
+    return num+t*2
+};
+// numとtの2つの整数が与えられたとき、次の操作を行うとnumと等しくなるなら、その数は達成可能である：
+
+// 数値を1増加または減少させ、同時にnumを1増加または減少させる。
+// この操作を最大 t 回行った後に達成可能な最大の数を返せ。
+// deepl
+// nanode tuneni num to tasseikanonusuuha 2 aku
+// sorewo t kai nanode sabun 2 kakete taseba ii
