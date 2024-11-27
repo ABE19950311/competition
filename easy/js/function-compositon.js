76aa859f19603821ca24821e02554d74668e5ddf
ABE19950311 <abe19950311@gmail.com>
2024-11-27 19:46:05 +0900

    1127

---
+++ easy/js/function-compositon.js
@@ -0,0 +1,23 @@
+var compose = function(functions) {
+    if(functions.length==0) {
+        return function(x) {
+            return x
+        }
+    } 
+
+    return functions.reduceRight(function(prevfn,nextfn) {
+        return function(x) {
+            return nextfn(prevfn(x))
+        }
+    })
+    
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+//関数わたしてごうせいしたかんすうにあたいわたしてきたいあたいくるようにする
+//reduceRightで右から左にしょりsてくれる
+//nextfnにx => x + 1 prevにx => 2 * x]
+//(2*x)+1 にごうせいする
